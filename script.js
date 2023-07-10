34bb5c2a1a914ad64971796a71efb3df35deaf93
nsandvig <135172168+nsandvig@users.noreply.github.com>
2023-07-10 15:18:36 -0500

    Update script.js
    Added the code. Small edits were made and should work, but will need tested.

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
+dragElement(document.getElementById('item1'));
+dragElement(document.getElementById('item2'));
+dragElement(document.getElementById('item3'));
+dragElement(document.getElementById('item4'));
+dragElement(document.getElementById('item5'));
+dragElement(document.getElementById('item6'));
+dragElement(document.getElementById('item7'));
+dragElement(document.getElementById('item8'));
 
+let max_Z_Index = 1
+
+function dragElement(bagElement) {
+	//set 4 positions for positioning on the screen
+	let pos1 = 0,
+		pos2 = 0,
+		pos3 = 0,
+		pos4 = 0;
+	bagElement.onpointerdown = pointerDrag;
+
+function pointerDrag(e) {
+	//increments the z index by 1
+	//edited out - max_Z_Index += 1;
+	//sets the current element's z index to the new top index
+	bagElement.style.zIndex = max_Z_Index;
+	e.preventDefault();
+	console.log(e);
+	pos3 = e.clientX;
+	pos4 = e.clientY;
+
+    document.onpointermove = elementDrag;
+    document.onpointerup = stopElementDrag;
+}
+
+function elementDrag(e) {
+	pos1 = pos3 - e.clientX;
+	pos2 = pos4 - e.clientY;
+	pos3 = e.clientX;
+	pos4 = e.clientY;
+	console.log(pos1, pos2, pos3, pos4);
+	bagElement.style.top = bagElement.offsetTop - pos2 + 'px';
+	bagElement.style.left = bagElement.offsetLeft - pos1 + 'px';
+}
+
+function stopElementDrag() {
+	document.onpointerup = null;
+	document.onpointermove = null;
+}
+}
